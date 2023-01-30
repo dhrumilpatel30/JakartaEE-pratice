228140f0fca6c50d2b44e6f4fcde65112a731833
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-01-30 20:58:31 +0530

    jakarta start

--- src/main/java/lab6/q2/ValidateServlet.java
+++ src/main/java/lab6/q2/ValidateServlet.java
@@ -15,16 +15,16 @@ public class ValidateServlet extends HttpServlet {
         response.setContentType("text/html");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost/student-registration", "root", "")) {
+        PrintWriter out = response.getWriter();
+        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/student-registration", "root", "")) {
             PreparedStatement ps = con.prepareStatement("select * from student where username=? and password=?");
             ps.setString(1,username);
             ps.setString(2,password);
             ResultSet rs = ps.executeQuery();
-            rs.isLast();
+            out.println(rs.isLast());
          } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-//        PrintWriter out = response.getWriter();
 //        out.println(username+password);
     }
 
