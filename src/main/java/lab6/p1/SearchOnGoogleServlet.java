5206c16c4c324ce0aad9430cab1245e88b83790d
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-02-02 21:10:45 +0530

    p1 add

--- src/main/java/lab6/p1/SearchOnGoogleServlet.java
+++ src/main/java/lab6/p1/SearchOnGoogleServlet.java
@@ -3,21 +3,26 @@ package lab6.p1;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import java.io.IOException;
 
-@WebServlet(name = "SearchOnGoogleServlet", value = "lab6/p1/SearchOnGoogleServlet")
+@WebServlet(name = "SearchOnGoogleServlet", value = "/lab6/p1/SearchOnGoogleServlet")
 public class SearchOnGoogleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String searchTerm = request.getParameter("search");
-        response.sendRedirect("https://www.google.co.in/#q="+searchTerm);
+        String searchURL = "https://www.google.com/search?q="+searchTerm;
+        Document doc =  Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
+        Elements links = doc.select("a[href]");
+        response.sendRedirect(links.attr("href"));
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String searchTerm = request.getParameter("search");
-        response.sendRedirect("https://www.google.co.in/#q="+searchTerm);
+        doGet(request,response);
     }
 }
