a20240f693d47d439c0292af15baa3fd5a9d8134
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-02-03 20:55:52 +0530

    p1 with google search api completed

--- src/main/java/lab6/p1/SearchOnGoogleServlet.java
+++ src/main/java/lab6/p1/SearchOnGoogleServlet.java
@@ -8,18 +8,19 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "SearchOnGoogleServlet", value = "/lab6/p1/SearchOnGoogleServlet")
 public class SearchOnGoogleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String searchTerm = request.getParameter("search");
+//        PrintWriter out = response.getWriter();
         String searchURL = "https://www.google.com/search?q="+searchTerm;
         Document doc =  Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
-        Elements links = doc.select("a[href]");
-        response.sendRedirect(links.attr("href"));
+        String search = doc.select("a[href]").get(1).attr("href");
+        response.sendRedirect("https://www.google.com"+search);
     }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request,response);
