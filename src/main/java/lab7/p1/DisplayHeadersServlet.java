75dde11ad69a93027c7ce6dceb406d4f41d1f944
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-02-12 18:30:29 +0530

    p1 strt

---
+++ src/main/java/lab7/p1/DisplayHeadersServlet.java
@@ -0,0 +1,25 @@
+package lab7.p1;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+@WebServlet(name = "DisplayHeadersServlet", value = "/lab7/p1/DisplayHeadersServlet")
+public class DisplayHeadersServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        Enumeration<String> headernames = request.getHeaderNames();
+        while (headernames.hasMoreElements()){
+            out.print(request.getHeader(headernames.nextElement()) + "<br>");
+        }
+    }
+}
