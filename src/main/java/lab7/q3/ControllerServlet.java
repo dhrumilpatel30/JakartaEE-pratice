85bd1f9a9ccc8dac8d05b236f725c1eaecc8daea
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-02-12 17:01:38 +0530

    q3 add

---
+++ src/main/java/lab7/q3/ControllerServlet.java
@@ -0,0 +1,20 @@
+package lab7.q3;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "ControllerServlet ", value = "/lab7/q3/ControllerServlet ")
+public class ControllerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+}
